ce58bdf2725ea4f00e631a06bae568580ad41a5d
muhammod1 <muhammodholawale@gmail.com>
2023-05-07 19:51:51 +0100

    adjustment

--- src/components/NavBarsec.jsx
+++ src/components/NavBarsec.jsx
@@ -12,32 +12,45 @@ const NavBarsec = ({ link }) => {
 
   return (
     <nav className="py-2 flex justify-between w-[97%] mx-auto max-w-[1430px]">
-      <Link to="/">
+      <Link to="/" className=" w-fit tablet laptop:w-[30%] laptop_l:w-[40%]">
         <img className=" h-[50px] mr-auto w-fit" src={logo} alt="logo" />
       </Link>
-      <div className="hidden laptop:flex text-[14px] text-blue gap-5 items-center font-semibold">
-        <NavLink to="/" className="py-3 px-5">
-          Home
-        </NavLink>
-        <a href="#content" className="py-3 px-5">
-          Content
-        </a>
-        <a href="#about" className="py-3 px-5">
-          About
-        </a>
+      <div className="w-fit tablet_l:w-[70%] laptop_l:w-[60%] hidden laptop:flex justify-between text-[14px] text-blue gap-5 items-center font-semibold">
+        <div>
+          <NavLink to="/" className="py-3 px-5">
+            Home
+          </NavLink>
+          <a href="#content" className="py-3 px-5">
+            Content
+          </a>
+          <a href="#about" className="py-3 px-5">
+            About
+          </a>
+        </div>
+        <div className=" hidden laptop:flex align-middle font-[500] text-[16px] text-grey">
+        <button
+          className="w-[200px] my-auto h-fit gap-10 bg-blue px-5 py-1 rounded-md hidden laptop:flex"
+          onClick={() => {
+            window.open("https://www.google.com");
+          }}
+        >
+          <BsSearch className="my-auto text-[#fff] w-[15px] h-[15px] " />
+          <p className="text-[#fff] ">Search</p>
+        </button>
+      </div>
       </div>
 
-      <div className=" hidden laptop:flex align-middle font-[500] text-[16px] text-grey">
-      <button
-        className="w-[200px] my-auto h-fit gap-10 bg-blue px-5 py-1 rounded-md hidden laptop:flex"
-        onClick={() => {
-          window.open("https://www.google.com");
-        }}
-      >
-        <BsSearch className="my-auto text-[#fff] w-[15px] h-[15px] " />
-        <p className="text-[#fff] ">Search</p>
-      </button>
-      </div>
+      {/* <div className=" hidden laptop:flex align-middle font-[500] text-[16px] text-grey">
+        <button
+          className="w-[200px] my-auto h-fit gap-10 bg-blue px-5 py-1 rounded-md hidden laptop:flex"
+          onClick={() => {
+            window.open("https://www.google.com");
+          }}
+        >
+          <BsSearch className="my-auto text-[#fff] w-[15px] h-[15px] " />
+          <p className="text-[#fff] ">Search</p>
+        </button>
+      </div> */}
 
       <button
         className="my-auto h-fit bg-blue hover:bg-bluep p-3 rounded-full laptop:hidden"
