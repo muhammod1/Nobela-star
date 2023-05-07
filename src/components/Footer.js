49d27137d6109db15c39f63be7b798ad84fcd85e
muhammod1 <muhammodholawale@gmail.com>
2023-05-07 18:36:26 +0100

    adjustment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,37 +9,72 @@ const Footer = () => {
   return (
     <div className="max-w-[1400px] w-full px-4 mx-auto pt-2 tab:pt-[5rem] pb-4 tab:pb-12">
       <div className="w-full mx-auto">
-      <div className="mt-6 flex pb-1 tab:pb-4 tablet:pb-8 border-[black] border-b-[1px] tab:border-b-2 justify-between">
-        <div className="">
-          <a href="https://www.figma.com/file/tPc7DcOMG7jqED3Fwxy96w/AI-Learning?node-id=2%3A3&t=P6B9gBagfN0XAZqy-0" className=" font-[700]  text-left w-fit text-[10px] mobile:text-[12px] tablet:text-[16px] leading-5">
-            Figma <p>Design</p>
-          </a>
-        </div>
-        <div>
-          <div className="relative bottom-2 flex gap-1.5 h-[50px]  text-[7.5px] tablet:text-[12px] font-semibold">
-            <p className="mt-auto">UNIVERSITY OF</p>
-            <img src={uniLogo} alt="" className=" relative tablet:bottom-0 bottom-0 w-[30px] mobile:w-[30px] h-[50px] mobile:h-[40px]" />
-            <p className="mt-auto">ILORIN, KWARA.</p>
+        <div className="mt-6 flex pb-1 tab:pb-4 tablet:pb-8 border-[black] border-b-[1px] tab:border-b-2 justify-between">
+          <div className="">
+            <a
+              href="https://www.figma.com/file/tPc7DcOMG7jqED3Fwxy96w/AI-Learning?node-id=2%3A3&t=P6B9gBagfN0XAZqy-0"
+              className=" font-[700]  text-left w-fit text-[10px] mobile:text-[12px] tablet:text-[16px] leading-5"
+            >
+              Figma <p>Design</p>
+            </a>
           </div>
-        </div>
+          <div>
+            <div className="relative bottom-2 flex gap-1.5 h-[50px]  text-[7.5px] tablet:text-[12px] font-semibold">
+              <p className="mt-auto">UNIVERSITY OF</p>
+              <img
+                src={uniLogo}
+                alt=""
+                className=" relative tablet:bottom-0 bottom-0 w-[30px] mobile:w-[30px] h-[50px] mobile:h-[40px]"
+              />
+              <p className="mt-auto">ILORIN, KWARA.</p>
+            </div>
+          </div>
 
-        <div>
-          <a href="https://github.com/NOBELA-STAR/Nobela-star" className="font-[700] text-[10px] mobile:text-[12px] tablet:text-[16px] flex flex-col justify-end leading-5">
-            <span className="w-fit ml-auto">Github</span> <span>Repository</span>
-          </a>
-        </div>
-      </div>
-      <div className="flex justify-center mt-4 tab:mt-10 tablet:mt-166">
-        <div>
-          <div className="mx-auto w-fit flex gap-2">
-            <img className="w-[10%] h-[15%]" src={git} alt="" />
-            <img className="w-[10%] h-[15%]"  src={twitter} alt="" />
-            <img className="w-[10%] h-[15%]"  src={figma} alt="" />
-            <img className="w-[10%] h-[15%]"  src={slack} alt="" />
+          <div>
+            <a
+              href="https://github.com/NOBELA-STAR/Nobela-star"
+              className="font-[700] text-[10px] mobile:text-[12px] tablet:text-[16px] flex flex-col justify-end leading-5"
+            >
+              <span className="w-fit ml-auto">Github</span>{" "}
+              <span>Repository</span>
+            </a>
           </div>
-          <p className="text-[10px] tablet:text-[10px] mt-4 font-semibold text-center">© Team, Fuad & Muhammod. 2023. We love Unilorin</p>
         </div>
-      </div>
+        <div className="flex justify-center mt-4 tab:mt-10 tablet:mt-166">
+          <div>
+          <div className="mx-auto w-fit flex gap-2">
+              <div
+                onClick={() => {
+                  window.open("https://github.com/muhammod1/Nobela-star");
+                }}
+                className="w-[15px] h-[15px] cursor-pointer"
+              >
+                <img className="w-full h-full" src={git} alt="" />
+              </div>
+              <div
+                onClick={() => {
+                  window.open("https://twitter.com/fuadUX");
+                }}
+                className="w-[15px] h-[15px] cursor-pointer"
+              >
+                <img className="w-full h-full" src={twitter} alt="" />
+              </div>
+              <div
+                onClick={() => {
+                  window.open(
+                    "https://www.figma.com/file/tPc7DcOMG7jqED3Fwxy96w/Learning-AI?type=design&node-id=140-4048&t=OAEFoQOEqtBzN6Mo-0"
+                  );
+                }}
+                className="w-[15px] h-[15px] cursor-pointer"
+              >
+                <img className="w-full h-full" src={figma} alt="" />
+              </div>
+            </div>
+            <p className="text-[10px] tablet:text-[10px] mt-4 font-semibold text-center">
+              © Team, Fuad & Muhammod. 2023. We love Unilorin
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/FooterSec.jsx
+++ src/components/FooterSec.jsx
@@ -9,37 +9,65 @@ const FooterSec = () => {
   return (
     <div className="max-w-[1400px] w-full bg-[#262626] px-4 mx-auto mt-20 pb-10">
       <div className="w-full mx-auto">
-      <div className="mt-6 flex pb-4 tablet:pb-8 border-[black] border-b-2 justify-between">
-        <div className="">
-          <div className=" font-[700]  text-left w-fit text-[10px] mobile:text-[14px] tablet:text-[16px] leading-5">
-            Figma <p>Design</p>
+        <div className="mt-6 flex pb-4 tablet:pb-8 border-[black] border-b-2 justify-between">
+          <div className="">
+            <div className=" font-[700]  text-left w-fit text-[10px] mobile:text-[14px] tablet:text-[16px] leading-5">
+              Figma <p>Design</p>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className="relative bottom-2 flex gap-1.5 h-[50px]  text-[7.5px] tablet:text-[12px] font-semibold">
-            <p className="mt-auto">UNIVERSITY OF</p>
-            <img src={uniLogo} alt="" className=" relative tablet:bottom-0 bottom-0 w-[30px] mobile:w-[30px] h-[50px] mobile:h-[40px]" />
-            <p className="mt-auto">ILORIN, KWARA.</p>
+          <div>
+            <div className="relative bottom-2 flex gap-1.5 h-[50px]  text-[7.5px] tablet:text-[12px] font-semibold">
+              <p className="mt-auto">UNIVERSITY OF</p>
+              <img
+                src={uniLogo}
+                alt=""
+                className=" relative tablet:bottom-0 bottom-0 w-[30px] mobile:w-[30px] h-[50px] mobile:h-[40px]"
+              />
+              <p className="mt-auto">ILORIN, KWARA.</p>
+            </div>
           </div>
-        </div>
 
-        <div>
-          <div className="font-[700] text-[10px] mobile:text-[14px] tablet:text-[16px] text-right leading-5">
-            Github <p>Repository</p>
+          <div>
+            <div className="font-[700] text-[10px] mobile:text-[14px] tablet:text-[16px] text-right leading-5">
+              Github <p>Repository</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex justify-center mt-10 tablet:mt-166">
-        <div>
-          <div className="mx-auto w-fit flex gap-2">
-            <img className="w-[10%] h-[15%]" src={git} alt="" />
-            <img className="w-[10%] h-[15%]" src={twitter} alt="" />
-            <img className="w-[10%] h-[15%]" src={figma} alt="" />
-            <img className="w-[10%] h-[15%]" src={slack} alt="" />
+        <div className="flex justify-center mt-10 tablet:mt-166">
+          <div>
+            <div className="mx-auto w-fit flex gap-2">
+              <div
+                onClick={() => {
+                  window.open("https://github.com/muhammod1/Nobela-star");
+                }}
+                className="w-[15px] h-[15px] cursor-pointer"
+              >
+                <img className="w-full h-full" src={git} alt="" />
+              </div>
+              <div
+                onClick={() => {
+                  window.open("https://twitter.com/fuadUX");
+                }}
+                className="w-[15px] h-[15px] cursor-pointer"
+              >
+                <img className="w-full h-full" src={twitter} alt="" />
+              </div>
+              <div
+                onClick={() => {
+                  window.open(
+                    "https://www.figma.com/file/tPc7DcOMG7jqED3Fwxy96w/Learning-AI?type=design&node-id=140-4048&t=OAEFoQOEqtBzN6Mo-0"
+                  );
+                }}
+                className="w-[15px] h-[15px] cursor-pointer"
+              >
+                <img className="w-full h-full" src={figma} alt="" />
+              </div>
+            </div>
+            <p className="text-[10px] tablet:text-[10px] mt-4 font-semibold text-center">
+              © Team, Fuad & Muhammod. 2023. We love Unilorin
+            </p>
           </div>
-          <p className="text-[10px] tablet:text-[10px] mt-4 font-semibold text-center">© Team, Fuad & Muhammod. 2023. We love Unilorin</p>
         </div>
-      </div>
       </div>
     </div>
   );
